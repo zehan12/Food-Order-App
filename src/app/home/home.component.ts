f470ab495975969038480fba36c8c0f6b84ce696
Zehan Khan <zehan9211@gmail.com>
2023-03-15 00:39:00 +0530

    injecting food service in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../sevices/food/food.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private fs: FoodService) { }
+  ngOnInit(): void {
+    
+  }
 }
