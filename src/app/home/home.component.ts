86605dd88aeb88e36f533fe0bb88dcb037e169af
Zehan Khan <zehan9211@gmail.com>
2023-03-15 17:53:06 +0530

    add acivatedRoute in home for search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../sevices/food/food.service';
 import { Foods } from '../shared/models/food';
 import { StarRatingComponent } from 'ng-starrating';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl:'./home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   foods: Foods[] = [];
-  constructor(private fs: FoodService) { }
+  constructor(private fs: FoodService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.foods = this.fs.getAll();
