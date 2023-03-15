4b5f55693ef57bc9ab729ff6c41b50292f96a731
Zehan Khan <zehan9211@gmail.com>
2023-03-16 01:28:33 +0530

    genrate tag component, set property, display tags and add css

---
+++ src/app/tags/tags.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../sevices/food/food.service';
+import { Tag } from '../shared/models/Tag';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.css']
+})
+export class TagsComponent implements OnInit {
+  tags: Tag[] = [];
+  constructor(private fs: FoodService) { }
+  ngOnInit(): void {
+    this.tags = this.fs.getAllTag()
+  }
+}
