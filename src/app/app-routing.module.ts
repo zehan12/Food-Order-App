60410dc21ba3ee373968de3878c4c9b30e8e5f54
Zehan Khan <zehan9211@gmail.com>
2023-03-16 22:42:35 +0530

    delete branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodpageComponent } from './foodpage/foodpage.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -11,6 +12,9 @@ const routes: Routes = [
   },
   {
     path: "tag/:tag", component: HomeComponent
+  },
+  {
+    path: "food/:id", component: FoodpageComponent
   }
 ];
 
