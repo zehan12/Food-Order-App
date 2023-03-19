62ecfdf79ec98ccdd76faaabae2bb38c46676d33
Zehan Khan <zehan9211@gmail.com>
2023-03-20 00:50:11 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors({
     credentials:true,
     origin:["http://localhost:4200"]
 }))
 
+app.get("/api",(req,res)=>{
+    res.end("hello from backend")
+})
 
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
