bd8d6df020d1253c2755ed31ce07958fc99e0492
Zehan Khan <zehan9211@gmail.com>
2023-03-20 00:47:12 +0530

    add cors origin to angular host

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
+
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}))
+
+
