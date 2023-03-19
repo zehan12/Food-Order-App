d0b975fb8581a1cac1b8d6dd97d2b1bec9621019
Zehan Khan <zehan9211@gmail.com>
2023-03-20 02:15:10 +0530

    remove ng

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require('path')
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +12,12 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }))
 
+app.use("/",express.static('../frontend/dist/foodorder'))
+
 app.get("/api",(req,res)=>{
     res.end("hello from backend")
 })
+
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}`)
