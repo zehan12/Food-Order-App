136e741b7b5c0ee3cda2a20ef3ba0cbf48a2519b
Zehan Khan <zehan9211@gmail.com>
2023-03-24 00:20:56 +0530

    IMPORTANT: ALL CHANGES REALATED TO DEPLOYMENT OF MEAN STACK APP

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -1,23 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
+
+import path from "path";
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    credentials:true,
-    origin:["http://localhost:4200"]
+    credentials: true,
+    origin: ["http://localhost:4200"]
 }))
 
 
-app.get("/",(req,res)=>{
+app.get("/api", (req, res) => {
     res.send("hello from backend")
 })
 
+app.use(express.static('public'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running on port ${port}`)
 })
---
+++ frontend/src/app/shared/constants/url.ts
@@ -0,0 +1,3 @@
+import { environment } from "src/app/enviorments/enviorment";
+
+const BASE_URL = environment.production ? "" : "http://localhost:3000";
